0f64fc09d9fe60ec8df2ab80f86bd42e5f35b880
abdellahozil <abdellah11ozil@gmail.com>
2025-05-14 12:28:57 +0100

    add custom content to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
-import Homepage from './containers/homepage';
+// import Homepage from './containers/homepage';
 
 function App() {
   return (
-      <Homepage/>
+      <div style={{textAlign:"center", paddingTop: "50px"}}>
+        <h1>Hello from Beema</h1>
+        <p>This is a working Github Pages React deployment</p>
+      </div>
     
   );
 }
