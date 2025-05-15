8c95a01e0add27d010cbf66112c7eabd0bb1e42b
abdellahozil <abdellah11ozil@gmail.com>
2025-05-15 09:06:39 +0100

    add new content in app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 
 import './App.css';
+import Homepage from './containers/homepage';
 // import Homepage from './containers/homepage';
 
 function App() {
   return (
-      <div style={{textAlign:"center", paddingTop: "50px"}}>
-        <h1>Hello from Beema</h1>
-        <p>This is a working Github Pages React deployment</p>
-      </div>
+      <Homepage/>
     
   );
 }
